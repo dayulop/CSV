38972bb22d5786bf41ec997a933e65e5b28b756d
dayulop <arielsantander28@gmail.com>
2023-03-18 20:46:21 -0300

    metodos creado preparado para llamarlos
    metodos creado, aun falta

--- Prueba1/src/main/java/modelo/DatoEquipo.java
+++ Prueba1/src/main/java/modelo/DatoEquipo.java
@@ -12,7 +12,7 @@ public class DatoEquipo {
     @CsvBindAndJoinByPosition(position = 3)
     private int Team1_Points;
     @CsvBindAndJoinByPosition(position = 4)
-    private int Team_Points;
+    private int Team2_Points;
     @CsvBindAndJoinByPosition(position = 5)
     private int Team2_id;
     @CsvBindAndJoinByPosition(position = 6)
@@ -20,6 +20,53 @@ public class DatoEquipo {
     @CsvBindAndJoinByPosition(position = 7)
     private String Team2_description;
 
-    
+    public int getTeam1_id(){
+        return Team1_id;
+    }
+    public void setTeam1_id(int id){
+        this.Team1_id = id;
+    }
+    public int getTeam1_Points(){
+        return Team1_Points;
+    }
+    public void setTeam1_Points(int points){
+        this.Team1_Points = points;
+    }
+    public String getTeam1_name(){
+        return Team1_name;
+    }
+    public void setTeam1_name(String name){
+        this.Team1_name = name;
+    }
+    public String getTeam1_description(){
+        return Team1_description;
+    }
+    public void setTeam1_description(String Description){
+        this.Team1_description = Description;
+    }
 
+    public int getTeam2_Points(){
+        return Team2_Points;
+    }
+    public void setTeam2_Points(int points){
+        this.Team2_Points = points;
+    }
+    public String getTeam2_name(){
+        return Team2_name;
+    }
+    public void setTeam2_name(String name){
+        this.Team2_name = name;
+    }
+    public String getTeam2_description(){
+        return Team2_description;
+    }
+    public void setTeam2_description(String Description){
+        this.Team2_description = Description;
+    }
+    public int getTeam2_id(){
+        return Team2_id;
+    }
+    public void setTeam2_id(int id){
+        this.Team2_id = id;
+    }
 }
--- Prueba1/src/main/java/org/example/Main.java
+++ Prueba1/src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 import com.opencsv.*;
+import com.opencsv.bean.CsvToBeanBuilder;
+import modelo.DatoEquipo;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if(args.length == 0){
+            System.out.println("ERROR: No ingresaste ningún archivo como argumento! volve a probar pibe");
+            System.exit(666);
+        }
+        List <DatoEquipo> listaEquipos;
+        try {
+            listaEquipos= (List<DatoEquipo>) new CsvToBeanBuilder(new FileReader(args[0]))
+                    .withType(DatoEquipo.class);
+                    .build()
+                    .parse();
+            
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
     }
+
 }
