8864a53c0f6319330f8f95e513e921565197979f
dayulop <arielsantander28@gmail.com>
2023-03-25 17:59:29 -0300

    lee los datos y los divide
    logre que el programa lea una linea de un archivo .csv y divida las primeras dos columnas para trabajarlas a parte

--- Prueba2/src/main/java/org/example/Main.java
+++ Prueba2/src/main/java/org/example/Main.java
@@ -17,41 +17,32 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.*;
 public class Main {
     public static void main(String[] args) throws Exception {
-        String archivo= "primer_partido";
+        CSVReader reader = new CSVReader(new FileReader(args[0]));
+        String[] nextLine= new String[1];//les puse de argumento un 1 pero creo que esta mal
+        String[] temporal = new String[1];
+        Integer[] columna1= new Integer[1];//guarda el id del equipo 1 de cada fila(cada fila equivale a un partido)
+        String[] columna2 = new String[1];//guarda el nombre del equipo 1 de cada fila
 
-        CSVReader reader = new CSVReaderBuilder(new FileReader("primer_partido.csv")).build();
-        String[] nextLine;
-        int[] columna1;
-        String[] columna2;
+        //TODO: se supone que entra toda la primer linea del archivo que mande. con split la va devolviendo por partes
+        try{
+            nextLine = reader.readNext();
+            String [] caracteres = new String[]{nextLine[0]};//tendria que ir aumentando el argumento de nexLine pero tengo una sola linea
 
-        nextLine = reader.readNext();
-        char [] caracteres = nextLine[0].toCharArray();
+            for(int i=0; i< caracteres.length;i++){//este for recorre las filas del texto
 
-        for(int i=0; i< caracteres.length;i++){
-            int minimo=0;
-            int maximo;
-            if( caracteres[i] == ){
+                temporal= caracteres[i].split(";");//guarda lo que hay en la fila separado por ";". asi se genera las columnas
 
+                for(int j=0;j< temporal.length;j++){//este for recorre las columnas del texto
+                    columna1[i]= Integer.parseInt(temporal[0]);
+                    columna2[i]= temporal[1];
+
+
+                }
+                System.out.println("id "+ columna1[i] + " nombre del equipo 1 " + columna2[i]);//esto solo es para probar que se guarde bien
             }
-
-
-
-            System.out.println(caracteres[i]);
+        }catch (Exception e){
+            System.out.println("algo fallo arriba  " + e);
         }
 
-
-
-
-
-
-
-
-
-        /*TODO try{ NO FUNCA NADA de lo que sigue
-            List <DatoResultados> beans = new CsvToBeanBuilder(new FileReader("primer_partido.csv"))
-                    .withType(DatoResultados.class).build().parse();
-        }catch (Exception e){
-            System.out.println(e);
-        }*/
     }
 }
