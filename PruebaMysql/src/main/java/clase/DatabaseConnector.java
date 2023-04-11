7d3f3a94d7e00fef95b9f259a0b1c8b828e2afbf
dayulop <arielsantander28@gmail.com>
2023-04-11 15:54:59 -0300

    creando_clase
    cree la clase de DBS.java para adaptar lo del nuevo tp integrador

---
+++ PruebaMysql/src/main/java/clase/DatabaseConnector.java
@@ -0,0 +1,72 @@
+package clase;
+import java.sql.*;
+
+import java.io.IOException;
+
+public class DatabaseConnector {
+    static final int PRIMERA_POSICION = 0;
+    static final int SEGUNDA_POSICION = 1;
+    static final int TERCERA_POSICION = 2;
+    static final int CUARTA_POSICION = 3;
+    static final int QUINTA_POSICION = 4;
+    static final int SEXTA_POSICION = 5;
+    static final int GANA_EQUIPO1 = 1;
+    static final int GANA_EQUIPO2 = 2;
+    static final int EMPATE = 0;
+    static final String INDICADOR = "X";
+
+    public void cargarPartidos(Competencia competencia) throws IOException {
+        Partido partidoLeido;
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pronosticador", "root", "");
+            Statement stmt = con.createStatement();
+
+            ResultSet rs= stmt.executeQuery("SELECT * FROM partidos");
+
+            while(rs.next()){
+                partidoLeido= new Partido(rs.getInt(PRIMERA_POSICION),
+                        rs.getInt(SEGUNDA_POSICION),
+                        rs.getString(TERCERA_POSICION),
+                        rs.getInt(CUARTA_POSICION),
+                        rs.getInt(QUINTA_POSICION),
+                        rs.getString(SEXTA_POSICION));
+
+                competencia.agregarPartido(partidoLeido);
+            }
+            com.close();
+        } cath(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void cargarPronosticos(Competencia competencia){
+        Pronostico pronosticoLeido;
+        int pronosticoPartido;
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pronosticador", "root", "");
+            Statement stmt = con.createStatement();
+            ResultSet rs   = stmt.executeQuery("SELECT * FROM partidos");
+
+            while(rs.next()){
+                pronosticoPartido = determinarPronosticoRealizado(//Todo cambiar tipo de argumento a recibir );
+                pronosticoLeido   = new Pronostico(rs.getInt(SEGUNDA_POSICION),rs.getInt(TERCERA_POSICION),pronosticoPartido);
+                competencia.agregarPronostico(pronosticoLeido);
+            }
+            com.close();
+        } cath(Exception e){
+            System.out.println(e);
+        }
+    }
+    private int determinarPronosticoRealizado(String[] caracteristicasPronostico){//todo cambiar la cadena de caracteres
+        int pronosticoRealizado;
+
+        if(caracteristicasPronostico[QUINTA_POSICION].equals(INDICADOR)) {
+            pronosticoRealizado = GANA_EQUIPO1;
+        } else if(caracteristicasPronostico[SEXTA_POSICION].equals(INDICADOR)){
+            pronosticoRealizado = EMPATE;
+        } else {
+            pronosticoRealizado = GANA_EQUIPO2;
+        }
+        return pronosticoRealizado;
+    }
+}
